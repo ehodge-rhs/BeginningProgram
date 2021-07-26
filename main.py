b2bc745a6a4985417cacfa706e4cdc0f2b72873b
MrHodge <56880443+ehodge-rhs@users.noreply.github.com>
2021-07-26 17:44:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+from turtle import Screen
+
+t: Turtle = Turtle()
+s: Screen = Screen()
+
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
